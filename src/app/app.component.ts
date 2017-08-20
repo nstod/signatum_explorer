ac70cbb96ffb128fe1b43a5da7ac177a1b422b61
Nate Stoddart <nstod11@gmail.com>
2017-08-20 11:41:21 -0600

    Support custom addresses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,16 @@ import { Wallet } from './wallet';
 import { Subscription } from "rxjs";
 import { Observable } from "rxjs/Rx";
 import { Coin } from './coin';
+import { CookieService } from 'ng2-cookies';
 
 @Component({
   selector: 'app-root',
-  providers: [ CryptopiaService, SignatumService ],
+  providers: [CryptopiaService, SignatumService, CookieService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit, OnDestroy {
-  constructor(
-    private cryptopiaService: CryptopiaService,
-    private signatumService: SignatumService)  {
-      this.wallets = [];
-      this.wallets.push(new Wallet('BMcrUorx7HZYYfmzpYkcR1zjAS4gUKwHmp'));
-      this.wallets.push(new Wallet('BEX5cYx3UJCYw4ATAxgaRSeyjyvWw2C18H'));
-      this.wallets.push(new Wallet('BS33RLFyKxqZFED16UGqCda83H3pPB1WVM'));
-    }
   balance = 0;
   sigt = new Coin(0, 0, 0);
   btc = new Coin(0, 0, 0);
@@ -29,6 +23,19 @@ export class AppComponent implements OnInit, OnDestroy {
   json = '';
   subscription: Subscription;
   wallets: Array<Wallet>;
+  cookies: Object;
+  addresses: string;
+  readonly cookieName = "sigtaddreses";
+
+  constructor(
+    private cryptopiaService: CryptopiaService,
+    private signatumService: SignatumService,
+    private cookieService: CookieService) {
+    if (this.cookieService.check(this.cookieName)) {
+      this.addresses = this.cookieService.get(this.cookieName);
+      this.updateWallets();
+    }
+  }
 
   ngOnInit() {
     let timer = Observable.timer(0, 10000);
@@ -45,6 +52,25 @@ export class AppComponent implements OnInit, OnDestroy {
     for (let wallet of this.wallets) {
       this.signatumService.getBalance(wallet.Address).subscribe(data => { wallet.Balance = data; this.updateValue(); });
     }
+  }
+
+  setAddresses(input: string) {
+    this.addresses = input;
+    this.cookieService.set(this.cookieName, this.addresses);
+    this.updateWallets();
+  }
+
+  updateWallets() {
+    this.wallets = [];
+    if (!this.addresses) {
+      return;
+    }
+
+    for (let address of this.addresses.split(',')) {
+      this.wallets.push(new Wallet(address));
+    }
+
+    this.calculate();
   }
 
   updateValue() {
